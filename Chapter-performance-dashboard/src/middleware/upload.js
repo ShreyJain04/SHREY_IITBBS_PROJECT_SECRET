dbccdb6eb23f763c085c7393cbdd89904a7b7f3e
ShreyJain04 <jainshrey200349@gmail.com>
2025-06-07 12:23:26 +0530

    Adjusted comments

--- Chapter-performance-dashboard/src/middleware/upload.js
+++ Chapter-performance-dashboard/src/middleware/upload.js
@@ -21,10 +21,10 @@ const fileFilter = (req, file, cb) => {
 };
 
 const upload = multer({
-  storage: multer.memoryStorage(),
+  storage:storage,
   fileFilter: fileFilter,
   limits: {
-    fileSize: 5 * 1024 * 1024 // 10MB limit
+    fileSize: 5 * 1024 * 1024 // 5MB limit
   }
 });
 
